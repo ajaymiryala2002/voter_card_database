0823ef1d6f3504d57a94750262eacea3c599cab6
ajaymiryala2002 <ajaymiryala259@gmail.com>
2025-12-06 09:30:33 +0530

    Fix static root & gunicorn

--- app1/models.py
+++ app1/models.py
@@ -5,14 +5,25 @@ def generate_voter_id():
     return "VTR" + uuid.uuid4().hex[:8].upper()
 
 class Voter(models.Model):
+    GENDER_CHOICES = (
+        ("Male", "Male"),
+        ("Female", "Female"),
+        ("Other", "Other"),
+    )
+
     full_name = models.CharField(max_length=100)
     father_name = models.CharField(max_length=100)
     dob = models.DateField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     address = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to="photos/", blank=True, null=True)
+
     voter_id_no = models.CharField(max_length=20, unique=True, blank=True)
-    photos = models.ImageField(upload_to="photos/", blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.voter_id_no:
             self.voter_id_no = generate_voter_id()
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.full_name
