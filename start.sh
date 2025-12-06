79d6ac4314d55260c1a11837c23fec2ce388b03b
ajaymiryala2002 <ajaymiryala259@gmail.com>
2025-12-06 10:10:11 +0530

    Fix static root & gunicorn

--- start.sh
+++ start.sh
@@ -1,4 +1,21 @@
+# #!/bin/bash
+# python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
+# gunicorn jango_voter_card.wsgi:application --bind 0.0.0.0:$PORT
+
 #!/bin/bash
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-gunicorn jango_voter_card.wsgi:application --bind 0.0.0.0:$PORT
+
+# Exit if any command fails
+set -o errexit  
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
+
+# Start server
+gunicorn jango_voter_card.wsgi:application --bind 0.0.0.0:8000
